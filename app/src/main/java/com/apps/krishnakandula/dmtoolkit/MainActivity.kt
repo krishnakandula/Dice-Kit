62cd7d3c158b5a0ac35646807edb1343d9420571
Krishna Kandula <krishna.c.kandula@gmail.com>
2018-07-04 16:06:33 -0500

    Add basic layouts for dice roller pad and template items

--- app/src/main/java/com/apps/krishnakandula/dmtoolkit/MainActivity.kt
+++ app/src/main/java/com/apps/krishnakandula/dmtoolkit/MainActivity.kt
@@ -2,11 +2,15 @@ package com.apps.krishnakandula.dmtoolkit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.apps.krishnakandula.dicerollerui.DiceRollerFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = DiceRollerFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
     }
 }
