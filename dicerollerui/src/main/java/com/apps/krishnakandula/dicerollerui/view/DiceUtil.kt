d2c0b98247c393093bd13dabab9bf17e18b42b29
Krishna Kandula <krishna.c.kandula@gmail.com>
2018-07-21 23:45:24 -0500

    Add vector drawables for dice

--- dicerollerui/src/main/java/com/apps/krishnakandula/dicerollerui/view/DiceUtil.kt
+++ dicerollerui/src/main/java/com/apps/krishnakandula/dicerollerui/view/DiceUtil.kt
@@ -2,6 +2,7 @@ package com.apps.krishnakandula.dicerollerui.view
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import com.apps.krishnakandula.dicerollercore.Dice
 import com.apps.krishnakandula.dicerollerui.R
 
@@ -22,3 +23,14 @@ fun List<Dice>.color(context: Context): Int {
 }
 
 fun Array<Dice>.color(context: Context): Int = this.toList().color(context)
+
+fun Dice.drawable(context: Context): Drawable {
+    return when (this) {
+        is Dice.D2 -> context.getDrawable(R.drawable.dice_d2)
+        is Dice.D4 -> context.getDrawable(R.drawable.dice_d4)
+        is Dice.D6 -> context.getDrawable(R.drawable.dice_d6)
+        is Dice.D8 -> context.getDrawable(R.drawable.dice_d8)
+        is Dice.D10 -> context.getDrawable(R.drawable.dice_d10)
+        is Dice.D20 -> context.getDrawable(R.drawable.dice_d20)
+    }
+}
